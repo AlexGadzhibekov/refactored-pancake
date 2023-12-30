ae8471e23d9ed84e86efc6d9a83484f07ecd80c1
AlexGazhibekov <cfifalex763@gmail.com>
2023-12-30 16:18:03 +0300

    create

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState('');
+
+  let options = {
+    storageOptions:{
+      path: 'image',
+    }
+  }
+  const ImagePicker = () => {
+    launchImageLibrary(options, response => {
+      setSelectedImage(response.assets[0].uri)
+    })
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <View style={{height:400, width:'100%'}}>
+        <Image style={{height:400, width:'100%'}} source={{uri:selectedImage}}/>
+      </View>
+    <TouchableOpacity style={styles.btn} onPress={() =>{
+      ImagePicker()
+    }}>
+      <Text> Upload photo </Text>
+    </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  btn:{
+    marginTop:20,
+    height:50,
+    width:'60%',
+    backgroundColor: 'skyblue',
+    borderRadius: 20,
     justifyContent: 'center',
-  },
+    alignItems:'center',
+    alignSelf:'center'
+  }
 });
